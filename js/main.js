5a659a48179895150aeb11dd8ee62e84b91080e3
Brian Francoeur <francoeurbj@gmail.com>
2022-09-15 11:46:40 -0400

    create html and add css

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-// Start writing JavaScript here!
+// create nav menu in index.html
+// -- Home
+// -- Work
+// -- About
+// -- Contact
+
+// hide menu by default. Use CSS to hide by default.
+
+// create button to show/hide off-canvas menu
+
+// create a click event listener
+
+// create CSS 'show' class
+
+// toggle 'show' class to display or hide the menu when the button is clicked
