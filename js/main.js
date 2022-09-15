ef482d3b51681dcc9507555a2b8fdc04d301dd65
Brian Francoeur <francoeurbj@gmail.com>
2022-09-15 13:28:24 -0400

    create js for off-canvas-menu and tweak to display correctly

--- js/main.js
+++ js/main.js
@@ -9,7 +9,15 @@
 // create button to show/hide off-canvas menu
 
 // create a click event listener
+const elemOffCanvasMenu = document.getElementById("off-canvas-menu");
+const elemBtnContainer = document.getElementById("btn-container");
+const elemToggleBtn = document.getElementById("menu-toggle-button");
 
+elemToggleBtn.addEventListener("click", () => {
+  event.preventDefault();
+  elemOffCanvasMenu.classList.toggle("show");
+  elemBtnContainer.classList.toggle("slide-right");
+});
 // create CSS 'show' class
 
 // toggle 'show' class to display or hide the menu when the button is clicked
